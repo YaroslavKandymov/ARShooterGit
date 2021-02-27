d8cd2cc2d20deafb33064485c39678a028e3e5af
Kandyimov Yaroslav <yakandyimov@gmail.com>
2021-02-27 17:19:56 +0300

    Init

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Enemy enemy))
+        {
+            enemy.Die();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    private int _score;
+
+    public event UnityAction<int> ScoreAdded;
+
+    public void AddScore()
+    {
+        _score++;
+        ScoreAdded?.Invoke(_score);
+    }
+}
